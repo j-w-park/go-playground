feea4741e26a9ca57a4c1cc650739331ffc532b9
j-w-park <okml133@gmail.com>
2023-10-11 02:53:23 +0900

    rename function

--- src/example/concurrency/4-range_and_close.go
+++ src/example/concurrency/4-range_and_close.go
@@ -2,30 +2,34 @@ package concurrency
 
 import "fmt"
 
-func fibonacci(n int, c chan int) {
-	x, y := 0, 1
-	for i := 0; i < n; i++ {
-		c <- x
-		x, y = y, x+y
-	}
-	close(c)
-}
+func ExampleRangeAndClose1() {
+	fmt.Println("ExampleRangeAndClose1")
 
-func Example4RangeAndClose1() {
-	fmt.Println("run ExampleRangeAndClose1")
+	fibonacci := func(n int, c chan int) {
+		x, y := 0, 1
+		for i := 0; i < n; i++ {
+			c <- x
+			x, y = y, x+y
+		}
+		close(c)
+	}
+
 	c := make(chan int, 10)
 	go fibonacci(cap(c), c)
+
 	for i := range c {
 		fmt.Println(i)
 	}
 }
 
-func Example4RangeAndClose2() {
-	fmt.Println("run ExampleRangeAndClose2")
+func ExampleRangeAndClose2() {
+	fmt.Println("ExampleRangeAndClose2")
+
 	queue := make(chan string, 2)
 	queue <- "one"
 	queue <- "two"
 	close(queue)
+
 	// This range iterates over each element as it’s received from queue.
 	// Because we closed the channel above, the iteration terminates after
 	// receiving the 2 elements.
